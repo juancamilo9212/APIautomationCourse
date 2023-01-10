06113c36c6661cac2feeed2e4f1e01bc7159f765
juancamilo9212 <camilocm_67@hotmail.com>
2023-01-10 12:20:56 -0500

    Exercise with 4 get requests, 4 post requests and 1 put request encapsulating business logic into a separate class from test layer

--- src/test/java/com/unosquare/Controller/ApiCore.java
+++ src/test/java/com/unosquare/Controller/ApiCore.java
@@ -1,13 +1,16 @@
 package com.unosquare.Controller;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.testng.Reporter;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -19,9 +22,23 @@ public class ApiCore {
         requestParams = new JSONObject();
     }
 
-    public Response postUser() {
-        requestParams.put("name", "JohnAPI");
-        requestParams.put("job", "QA");
+    public Response getAllUsers(){
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.headers("Content-Type", "application/json");
+        Response response = httpRequest.get("users/");
+        return response;
+    }
+
+    public Response getSpecificUser(int id){
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.headers("Content-Type", "application/json");
+        Response response = httpRequest.get("users/"+Integer.toString(id));
+        return response;
+    }
+
+    public Response postUser(String name,String job) {
+        requestParams.put("name", name);
+        requestParams.put("job", job);
         RequestSpecification httpRequest = RestAssured.given();
         httpRequest.headers("Content-Type", "application/json");
         httpRequest.body(requestParams.toString());
@@ -29,23 +46,39 @@ public class ApiCore {
         return response;
     }
 
-    public void addMessagesToReport(Response response) {
+    public void addMessagesToReport(Response response,String url) {
         Reporter.log("Response Body " + response.prettyPrint());
         Reporter.log("Response code " + response.getStatusCode());
         Reporter.log("Request Body " + requestParams.toString());
-        Reporter.log("Request URL " + "https://reqres.in/api/users/");
+        Reporter.log("Request URL " + url);
     }
 
-    public Response postUserFromFile(String filePath) throws IOException, ParseException {
+    public Response createUserFromFile(String filePath,String url) throws IOException, ParseException {
+        Response response = this.sendHttpRequestFromFile(filePath,"post",url);
+        return response;
+    }
+
+    public Response updateUserFromFile(String filePath,String url,int id) throws IOException, ParseException {
+        Response response = this.sendHttpRequestFromFile(filePath,"put",url+Integer.toString(id));
+        return response;
+    }
+
+    public void convertJsonFromFile(String filePath) throws IOException, ParseException {
+        JSONParser json = new JSONParser();
+        FileReader reader = new FileReader(filePath);
+        Object requestParams = json.parse(reader);
+    }
+
+    public Response sendHttpRequestFromFile(String filePath,String method,String url){
         Response response = null;
         try {
-            JSONParser json = new JSONParser();
-            FileReader reader = new FileReader(filePath);
-            Object requestParams = json.parse(reader);
+            this.convertJsonFromFile(filePath);
             RequestSpecification httpRequest = RestAssured.given();
             httpRequest.headers("Content-Type", "application/json");
             httpRequest.body(requestParams.toString());
-            response = httpRequest.post("/users");
+            response = method.equalsIgnoreCase("post") ?
+                    httpRequest.post(url):
+                    httpRequest.put(url);
         } catch (IOException exception) {
             exception.printStackTrace();
         } catch (ParseException exception) {
@@ -53,4 +86,10 @@ public class ApiCore {
         }
         return response;
     }
+
+    public String getValueFromJson(Response response,String attribute){
+        JsonPath jsonPath = new JsonPath(response.asString());
+        return jsonPath.getString(attribute);
+    }
+
 }
--- src/test/java/com/unosquare/Controller/Constants.java
+++ src/test/java/com/unosquare/Controller/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String BASE_URL = "https://reqres.in/api/";
     public static final String FILE_PATH = "src/test/java/com/unosquare/resources/Register.json";
+    public static final String FILE_PATH2 = "src/test/java/com/unosquare/resources/Register2.json";
+    public static final String USERS_URL = "users/";
 }
